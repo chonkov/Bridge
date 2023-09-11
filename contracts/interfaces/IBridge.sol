2897b076ec230dd2072b7707dd4ec06cbd059afd
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-09-11 12:22:15 +0300

    Added interface for 'Bridge' contract

---
+++ contracts/interfaces/IBridge.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IBridge {
+    function registerToken(address _token) external;
+
+    function deployWrappedToken(
+        address _token,
+        string memory _name,
+        string memory _symbol
+    ) external;
+
+    function lockToken(
+        address _token,
+        uint256 _amount,
+        bytes calldata _signature
+    ) external payable;
+
+    function release(address _token, address _to, uint256 _amount) external;
+
+    function burn(address _token, uint256 _amount, uint _nonce) external;
+
+    function claim(
+        address _token,
+        address from,
+        address to,
+        uint amount,
+        uint nonce,
+        bytes calldata signature
+    ) external;
+}
