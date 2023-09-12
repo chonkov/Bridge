eb4b1f74f73800a10633cc3d6414bc195530552e
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro-2.local>
2023-09-12 11:24:43 +0300

    Updated config - local nodes can be spin up, forking testnets and after that - scripts/tests run on them. Added also '.env' files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost_sepolia: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8545/",
+    },
+    localhost_mumbai: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8546/",
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      chainId: 80001,
+      url: MUMBAI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+  },
 };
