652b891d3884682d49f1eac8f39005efba3c12d5
Georgi Chonkov <georgi.chonkov@limechain.tech>
2023-09-27 13:41:27 +0300

    Added 'Firebase' db to store emitted events by the deployed bridges

--- scripts/eventListener.js
+++ scripts/eventListener.js
@@ -2,6 +2,24 @@ const { ethers } = require("hardhat");
 const Bridge = require("../artifacts/contracts/Bridge.sol/Bridge.json");
 const PermitToken = require("../artifacts/contracts/ERC20PermitToken.sol/ERC20PermitToken.json");
 // const Token = require("../artifacts/contracts/ERC20Token.sol/ERC20Token.json");
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  collection,
+  // getDocs,
+  addDoc,
+  onSnapshot,
+} = require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCa9IPhOXinFWPcsee5BEHQcRJN-BXutfE",
+  authDomain: "bridge-d2101.firebaseapp.com",
+  projectId: "bridge-d2101",
+  storageBucket: "bridge-d2101.appspot.com",
+  messagingSenderId: "90712161589",
+  appId: "1:90712161589:web:adb078a0bc31cda1ffe07c",
+  measurementId: "G-X6T0BF8L5W",
+};
 
 async function main() {
   const name = "USD Coin";
@@ -15,9 +33,20 @@ async function main() {
     "http://localhost:8546"
   );
 
-  const permitTokenAddr = "";
-  const bridgeSepoliaAddr = "";
-  const bridgeMumbaiAddr = "";
+  const permitTokenAddr = "0x7C969786F2477851cf2B1b05b4A9D369f3C37140";
+  const bridgeSepoliaAddr = "0xEE79588a506240e95e25ebABA51389e1e8788058";
+  const bridgeMumbaiAddr = "0x575deA2cCAc5B962E7830c49DeC47e28aB83376C";
+
+  initializeApp(firebaseConfig);
+  const db = getFirestore();
+  const colRef = collection(db, "events");
+  onSnapshot(colRef, (snapshot) => {
+    const events = [];
+    snapshot.docs.forEach((doc) => {
+      events.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(events);
+  });
 
   const permitToken = new ethers.Contract(
     permitTokenAddr,
@@ -56,17 +85,48 @@ async function main() {
     (_token, _from, _chainId, _amount, _deadline, _signature) => {
       console.log("###########");
       console.log(_token, _from, _chainId, _amount, _deadline, _signature);
+
+      const data = {
+        eventType: "Lock",
+        token: _token,
+        from: _from,
+        chainId: `${_chainId}`,
+        amount: `${_amount}`,
+        deadline: `${_deadline}`,
+        signature: _signature,
+      };
+
+      addDoc(colRef, data).then(() => {});
     }
   );
 
   bridgeSepolia.on("ReleaseToken", (_token, _to, _amount) => {
     console.log("###########");
     console.log(_token, _to, _amount);
+
+    const data = {
+      eventType: "Release",
+      token: _token,
+      to: _to,
+      amount: `${_amount}`,
+    };
+
+    addDoc(colRef, data).then(() => {});
   });
 
   bridgeMumbai.on("DeployToken", (_token, _wrapper, _name, _symbol) => {
     console.log("###########");
     console.log(_token, _wrapper, _name, _symbol);
+
+    const data = {
+      eventType: "Deploy",
+      token: _token,
+      wrapper: _wrapper,
+      name: _name,
+      symbol: _symbol,
+    };
+
+    addDoc(colRef, data).then(() => {});
   });
 
   bridgeMumbai.on(
@@ -74,12 +134,36 @@ async function main() {
     (_token, _from, _to, _chainId, _amount, _nonce, _signature) => {
       console.log("###########");
       console.log(_token, _from, _to, _chainId, _amount, _nonce, _signature);
+
+      const data = {
+        eventType: "Claim",
+        token: _token,
+        from: _from,
+        to: _to,
+        chainId: `${_chainId}`,
+        amount: `${_amount}`,
+        nonce: `${_nonce}`,
+        signature: _signature,
+      };
+
+      addDoc(colRef, data).then(() => {});
     }
   );
 
   bridgeMumbai.on("BurnToken", (_token, _sender, _chainId, _amount, _nonce) => {
     console.log("###########");
     console.log(_token, _sender, _chainId, _amount, _nonce);
+
+    const data = {
+      eventType: "Burn",
+      token: _token,
+      sender: _sender,
+      chainId: `${_chainId}`,
+      amount: `${_amount}`,
+      nonce: `${_nonce}`,
+    };
+
+    addDoc(colRef, data).then(() => {});
   });
 }
 
