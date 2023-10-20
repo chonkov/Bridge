2bd46d022596024ad1b7ea7d4d29c1ce964410c6
Georgi Chonkov <georgi.chonkov@limechain.tech>
2023-10-20 14:43:57 +0300

    Refactored 'bin' file

--- cli/bin.js
+++ cli/bin.js
@@ -1,54 +1,15 @@
 #! /usr/bin/env node
 require("dotenv").config();
-const { ethers } = require("ethers");
+const { ethers } = require("hardhat");
 const yargs = require("yargs");
-const fs = require("fs");
-const bridgeAbi = require("../artifacts/contracts/Bridge.sol/Bridge.json").abi;
-const permitTokenAbi =
-  require("../artifacts/contracts/ERC20PermitToken.sol/ERC20PermitToken.json").abi;
-
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
-const PERMIT_TOKEN = process.env.PERMIT_TOKEN || "0x";
-const BRIDGE_SEPOLIA = process.env.BRIDGE_SEPOLIA || "0x";
-const BRIDGE_MUMBAI = process.env.BRIDGE_MUMBAI || "0x";
-
-// const sepoliaForkProvider = new ethers.JsonRpcProvider(
-//   "http://127.0.0.1:8545/"
-// );
-// const mumbaiForkProvider = new ethers.JsonRpcProvider("http://127.0.0.1:8546/");
-
-// Redundant
-// const sepoliaWallet = new ethers.Wallet(
-//   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // first key of the provided by hardhat
-//   sepoliaForkProvider
-// );
-// const mumbaiWallet = new ethers.Wallet(
-//   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // first key of the provided by hardhat
-//   mumbaiForkProvider
-// );
-
-// let sourceSigner = new ethers.Wallet(USER_KEY, sourceProvider);
-// let destinationSigner = new ethers.Wallet(USER_KEY, destinationProvider);
-
-// 1. npx hardhat run scripts/deployERC20PermitToken.js --network localhost_<SOURCE_CHAIN>
-// 2. npx hardhat run scripts/deployBridge.js --network localhost_<SOURCE_CHAIN>
-// 3. npx hardhat run scripts/deployBridge.js --network localhost_<DESTINATION_CHAIN>
-
-// 4. npx hardhat permit-signature --from <SIGNER_ADDRESS> --to <SOURCE_BRIDGE_STEP_2> --token <SOURCE_TOKEN_STEP_1> --amount <TOKENS_AMOUNT> --deadline <DEADLINE> --network localhost_<SOURCE_CHAIN>
-// 5. npx hardhat lock --bridge <SOURCE_BRIDGE_STEP_2> --token <SOURCE_TOKEN_STEP_1> --amount <TOKENS_AMOUNT> --deadline <DEADLINE> --fee 1000000000000000000 --signature <SIGNATURE_COMPUTED_STEP_4> --network localhost_<SOURCE_CHAIN>
-
-// 6. npx hardhat signature --from <SIGNER_ADDRESS> --to <RECEIVER_ADDRESS> --amount <TOKENS_AMOUNT> --deadline <DEADLINE> --nonce <NONCE_NOT_ALREADY_PROCESSED> --network localhost_<DESTINATION_CHAIN>
-// 7. npx hardhat claim --from <SIGNER_ADDRESS> --to <RECEIVER_ADDRESS> --bridge <DESTINATION_BRIDGE_STEP_3> --token <SOURCE_TOKEN_STEP_1> --name <NAME> --symbol <SYMBOL> --amount <TOKENS_AMOUNT> --deadline <DEADLINE> --nonce <NONCE_NOT_ALREADY_PROCESSED> --signature <SIGNATURE_COMPUTED_STEP_6> --network localhost_<DESTINATION_CHAIN>
-
-// 8. npx hardhat burn --bridge <DESTINATION_BRIDGE_STEP_3> --token <INDEX_OF_TOKEN> --amount <TOKENS_AMOUNT> --nonce <NONCE_NOT_ALREADY_PROCESSED> --network localhost_<DESTINATION_CHAIN>
-// 9. npx hardhat release --to <RECEIVER_ADDRESS> --bridge <SOURCE_BRIDGE_STEP_2> --token <SOURCE_TOKEN_STEP_1> --amount <TOKENS_AMOUNT> --network localhost_<SOURCE_CHAIN>
+const bridgeAbi = require("../artifacts/contracts/Bridge.sol/Bridge.json");
+const permitTokenAbi = require("../artifacts/contracts/ERC20PermitToken.sol/ERC20PermitToken.json");
+const { getPermitSignature } = require("../utils/getPermitSignature");
 
 const options = yargs
   .usage("Select action to perform on the bridge.")
   .command(
-    "lock <bridge_address> <token_address> <amount> <deadline> <fee> <signature> <chain>",
+    "lock bridge_address token_address amount deadline fee signature chain",
     "Lock amount of funds in bridge.",
     (yargs) => {
       yargs.positional("bridge_address", {
@@ -83,7 +44,7 @@ const options = yargs
     }
   )
   .command(
-    "claim <from> <to> <bridge_address> <token_address> <name> <symbol> <amount> <deadline> <nonce> <signature> <chain>",
+    "claim from to bridge_address token_address name symbol amount deadline nonce signature chain",
     "Claim amount of funds",
     (yargs) => {
       yargs.positional("from", {
@@ -134,7 +95,7 @@ const options = yargs
     }
   )
   .command(
-    "burn <bridge_address> <token> <amount> <nonce> <chain>",
+    "burn bridge_address token amount nonce chain",
     "Burn funds",
     (yargs) => {
       yargs.positional("bridge_address", {
@@ -161,7 +122,7 @@ const options = yargs
     }
   )
   .command(
-    "release <to> <bridge_address> <token_address> <amount> <chain>",
+    "release to bridge_address token_address amount chain",
     "Release locked funds on source chain.",
     (yargs) => {
       yargs.positional("to", {
@@ -187,8 +148,8 @@ const options = yargs
     }
   )
   .command(
-    "permit-signature <from> <to> <token> <amount> <deadline> <chain>",
-    "Compute signature.",
+    "permit-signature from to token amount deadline chain",
+    "Compute permit signature.",
     (yargs) => {
       yargs.positional("from", {
         describe: "The address wbich initiated the tx",
@@ -213,7 +174,7 @@ const options = yargs
     }
   )
   .command(
-    "signature <from> <to> <amount> <deadline> <nonce> <chain>",
+    "signature from to amount deadline nonce chain",
     "Compute signature.",
     (yargs) => {
       yargs.positional("from", {
@@ -248,19 +209,6 @@ const options = yargs
 const command = options._[0];
 
 async function main() {
-  // let token = options.address;
-  // let amount = options.amount;
-  // let wERC20;
-  // let result;
-  // let r = options.r;
-  // let s = options.s;
-  // let v = options.v;
-  // <bridge_address> <token_address> <amount> <deadline> <fee> <signature> <chain>
-  // <from> <to> <bridge_address> <token_address> <name> <symbol> <amount> <deadline> <nonce> <signature> <chain>
-  // <bridge_address> <token_address> <amount> <nonce> <chain>
-  // <to> <bridge_address> <token_address> <amount> <chain>
-  // <from> <to> <token> <amount> <deadline> <chain>
-
   const [signer] = await ethers.getSigners();
 
   const bridgeAddress = options.bridge_address;
@@ -288,42 +236,60 @@ async function main() {
       break;
   }
 
+  console.log(`${JSON.stringify(provider)}`);
   const provider = new ethers.JsonRpcProvider(url);
 
   const permitToken = new ethers.Contract(
     tokenAddress,
-    permitTokenAbi,
+    permitTokenAbi.abi,
     provider
   );
 
-  const bridgeSepolia = new ethers.Contract(bridgeAddress, bridgeAbi, provider);
-
-  const bridgeMumbai = new ethers.Contract(bridgeAddress, bridgeAbi, provider);
+  const bridge = new ethers.Contract(bridgeAddress, bridgeAbi.abi, provider);
 
   switch (command) {
     case "lock":
-      await bridgeSepolia.lockToken(tokenAddress, amount, deadline, signature, {
-        value: fee,
-      });
+      await bridge
+        .connect(signer)
+        .lock(tokenAddress, amount, deadline, signature, {
+          value: fee,
+        });
       console.log(`✅ Tokens are locked`);
       break;
     case "claim":
-      // Add logic
+      await bridge
+        .connect(signer)
+        .claim(tokenAddress, name, symbol, from, to, amount, nonce, signature);
+      console.log(`✅ Wrapped token are claimed`);
       break;
     case "burn":
-      // Add logic
+      await bridge.connect(signer).burn(tokenIndex, amount, nonce);
+      console.log(`✅ Wrapped token are burnt`);
       break;
     case "release":
-      // Add logic
+      await bridge
+        .connect(signer)
+        .release(permitToken.target, signer.address, amount);
+      console.log(`✅ Tokens are released back to owner`);
       break;
     case "permit-signature":
-      console.log("Permit signature...");
+      const permitSignature = await getPermitSignature(
+        signer,
+        permitToken,
+        to,
+        amount,
+        deadline
+      );
+      console.log(permitSignature);
       break;
     case "signature":
-      console.log("Signature...");
-      break;
-    case "demo":
-      console.log(`Demo command executed ✅`);
+      const bytes = ethers.solidityPacked(
+        ["address", "address", "uint256", "uint256"],
+        [from, to, amount, nonce]
+      );
+      const hash = ethers.keccak256(bytes);
+      const signature = await signer.signMessage(ethers.toBeArray(hash));
+      console.log(signature);
       break;
     default:
       // Handle invalid or unsupported commands
